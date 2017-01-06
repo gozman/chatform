b5a089658cd83fc4eb0b7aa7a631bcdc3b5a2662
mike gozzo <mgozzo@gmail.com>
2017-01-06 13:53:23 -0500

    quick replies in form builder

--- public/js/main.js
+++ public/js/main.js
@@ -4,7 +4,18 @@ $(document).ready(function() {
   var formFields = JSON.parse(unescape($("#fieldData").val()));
 
   for(var i=0; i<formFields.length; i++) {
-    var questionElement = "<div class='question-panel'><a class='btn btn-remove pull-right'>Remove Question</a><textarea rows='2' cols='85' placeholder='Add your question here'>" + formFields[i].question + "</textarea>"
+    var questionElement = $("<div class='question-panel'><a class='btn btn-remove pull-right'>Remove Question</a><textarea rows='2' cols='85' placeholder='Add your question here'>" + formFields[i].question + "</textarea><div class='quick-reply-field'><input type='text' class='reply-text' placeholder='Quick reply'/><a class='add-answer'>+</a><a class='remove-answer'>-</a></div></div>")
+
+    //Add in any quick replies
+    for(var j=0; j<formFields[i].answers.length; j++) {
+      var lastReplyField = questionElement.find('.reply-text:last');
+      lastReplyField.val(formFields[i].answers[j]);
+
+      if( (j+1) != formFields[i].answers.length ) {
+        $("<div class='quick-reply-field'><input type='text' class='reply-text' placeholder='Quick reply'/><a class='add-answer'>+</a><a class='remove-answer'>-</a></div>").insertAfter(lastReplyField.parent());
+      }
+    }
+
     if(i==0) {
       //First form field, insert after start trigger
       $(questionElement).insertAfter("#startMessage");
@@ -15,7 +26,7 @@ $(document).ready(function() {
 
   $("#add_text_question").click(function() {
     console.log("Hello!@");
-    var questionElement = "<div class='question-panel'><a class='btn btn-remove pull-right'>Remove Question</a><textarea rows='2' cols='85' placeholder='Add your question here'></textarea>"
+    var questionElement = "<div class='question-panel'><a class='btn btn-remove pull-right'>Remove Question</a><textarea rows='2' cols='85' placeholder='Add your question here'></textarea><div class='quick-reply-field'><input type='text' class='reply-text' placeholder='Quick reply'/><a class='add-answer'>+</a><a class='remove-answer'>-</a></div></div>"
     if($('.question-panel').length) {
       $(questionElement).insertAfter(".question-panel:last");
     } else {
@@ -24,6 +35,15 @@ $(document).ready(function() {
   });
 
   $('.right-column').on('click', '.btn-remove', function(event) {
+    var par = $(event.target).parent();
+    par.remove();
+  });
+
+  $('.right-column').on('click', '.add-answer', function(event) {
+    $("<div class='quick-reply-field'><input type='text' class='reply-text' placeholder='Quick reply'/><a class='add-answer'>+</a><a class='remove-answer'>-</a></div>").insertAfter($(event.target).parent());
+  });
+
+  $('.right-column').on('click', '.remove-answer', function(event) {
     var par = $(event.target).parent();
     par.remove();
   });
@@ -42,6 +62,17 @@ $(document).ready(function() {
         type: 'text'
       }
 
+      //Get quick reply buttons, if they exist
+      var parent = $(this).parent();
+      var qr = [];
+      parent.find('.quick-reply-field').each(function(index) {
+        var replyText = $(this).find('.reply-text').val();
+        if(replyText) {
+          qr.push(replyText);
+        }
+      });
+
+      field.answers = qr;
       fields.push(field);
     });
     $("input[name='fields']").val(JSON.stringify(fields));
